8dcfd3cd1c8267ec9b45fe6c6a7308fd46bbee05
Zakovriashin Timofei <tzakovriashin@gmail.com>
2024-08-09 00:49:27 +0300

    third iteration

--- ftracker.go
+++ ftracker.go
@@ -59,7 +59,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
         return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
     case trainingType == "Плавание":
         distance := distance(action)
-        speed := meanSpeed(action, duration)
+        speed := swimmingMeanSpeed(lengthPool, countPool , duration)
         calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight)
         return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
     default:
@@ -82,7 +82,7 @@ const (
 // duration float64 — длительность тренировки в часах.
 func RunningSpentCalories(action int, weight, duration float64) float64 {
     // ваш код здесь
-    return (runningCaloriesMeanSpeedMultiplier * ((float64(action)*lenStep)/duration) * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * float64(minInH)  
+    return (runningCaloriesMeanSpeedMultiplier * ((float64(action)*lenStep)/mInKm/duration) * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * float64(minInH)  
 }
 
 // Константы для расчета калорий, расходуемых при ходьбе.
